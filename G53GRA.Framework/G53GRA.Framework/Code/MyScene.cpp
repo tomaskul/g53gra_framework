01f8951381f92dc0c147af56aca1885504866c89
Tomas <psytk2@nottingham.ac.uk>
2019-02-08 17:42:34 +0000

    Adding Triangle demo

--- G53GRA.Framework/G53GRA.Framework/Code/MyScene.cpp
+++ G53GRA.Framework/G53GRA.Framework/Code/MyScene.cpp
@@ -1,5 +1,7 @@
 #include "MyScene.h"
 
+#include <Demos/Triangle/Triangle.h>
+
 MyScene::MyScene(int argc, char** argv, const char *title, const int& windowWidth, const int& windowHeight)
 	: Scene(argc, argv, title, windowWidth, windowHeight)
 {
@@ -9,6 +11,12 @@ MyScene::MyScene(int argc, char** argv, const char *title, const int& windowWidt
 void MyScene::Initialise()
 {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	// Demos
+
+	// Triangle
+	Triangle *t = new Triangle();
+	AddObjectToScene(t);
 
 }
 
