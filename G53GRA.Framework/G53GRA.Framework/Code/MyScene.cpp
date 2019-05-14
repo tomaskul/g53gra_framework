fa6b94888402239cc829c83155ee2dd1ca5bd51d
Tomas <psytk2@nottingham.ac.uk>
2019-05-14 18:08:08 +0100

    Updating contents of the scene

--- G53GRA.Framework/G53GRA.Framework/Code/MyScene.cpp
+++ G53GRA.Framework/G53GRA.Framework/Code/MyScene.cpp
@@ -1,24 +1,34 @@
 #include <Train/Locomotive.h>
-#include <Landscape/Tree/Tree.h>
+#include <Environment/Tree/Tree.h>
+#include <World/Skybox.h>
+#include <Environment/Comet.h>
 #include "MyScene.h"
 
 MyScene::MyScene(int argc, char** argv, const char *title, const int& windowWidth, const int& windowHeight)
 	: Scene(argc, argv, title, windowWidth, windowHeight)
 {
-
 }
 
 void MyScene::Initialise()
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	Locomotive *locomotive = new Locomotive();
+	const std::string textureDirectory = "";
+
+	auto *skybox = new Skybox(textureDirectory + "skybox/");
+	AddObjectToScene(skybox);
+
+	auto *locomotive = new Locomotive();
 	AddObjectToScene(locomotive);
 
-	GLuint trunk = Scene::GetTexture("");
-	Tree *tree = new Tree(0,0,15,trunk, 0);
+
+	GLuint trunk = Scene::GetTexture(textureDirectory + "TexturesCom_BarkDecidious0026_S.bmp");
+	Tree *tree = new Tree(0, 0, 0, trunk, 0);
 	AddObjectToScene(tree);
 
-	glClearColor(1.0f, 0.5f, 0.25f, 1.0f);
+	auto *comet = new Comet(5, 2, 15);
+	AddObjectToScene(comet);
+
+	glClearColor(.0f, .0f, .0f, 0.5f);
+    //glClearColor(1.0f, 0.5f, 0.25f, 1.0f);
 }
 
 void MyScene::Projection()
